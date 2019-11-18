8ea80e7663f604a5d2887e6b0a4c36ed49d53cba
lucidmachine <lucid.machine@gmail.com>
2019-11-17 22:17:23 -0500

    Move bash configuration to subdirectory.

---
+++ bash/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# Sets up the symlinks to the bash config files.
+
+# Symlink bash dotfiles
+for dotfile in bash_aliases bash_logout bashrc profile; do
+    echo ""
+    
+    # Check whether to delete existing copies
+    if [ -f "${HOME}/.${dotfile}" ]; then
+
+        # Prompt
+        echo "${HOME}/.${dotfile} already exists. Overwrite? (y/N): "
+        read overwrite
+
+        if [ "${overwrite}" != "y" ] && [ "${overwrite}" != "Y" ]; then
+            # Don't delete
+            continue
+        else
+            # Delete
+            rm "${HOME}/.${dotfile}"
+        fi
+    fi
+
+    # Create a symlink in home pointing to the dotfile in this directory
+    bash_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    ln -s "${bash_dir}/${dotfile}" "${HOME}/.${dotfile}"
+
+    # Source the new file
+    . "${HOME}/${dotfile}"
+
+done
