ea26e48748e479b38092853e39b03e8cdeec921f
Eric Slenk <lucid.machine@gmail.com>
2024-06-02 22:33:20 -0400

    nvim: Separate options config

---
+++ neovim/lua/options.lua
@@ -0,0 +1,82 @@
+-- Backspace
+vim.opt.backspace = 'indent,eol,start'
+
+-- Document Formatting
+vim.o.autoindent = true
+vim.o.conceallevel = 0
+vim.o.wrap = false
+vim.o.smarttab = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.opt.formatoptions = {
+  -- See: :h fo-table
+  c = true,
+  j = true,
+  l = true,
+  o = true,
+  q = true,
+  r = true,
+}
+
+-- Filetypes
+vim.cmd([[
+  filetype on
+  filetype plugin on
+  filetype indent on
+  syntax on
+]])
+vim.api.nvim_create_autocmd(
+  { 'FileType' },
+  {
+    pattern = 'make',
+    command = 'setlocal noexpandtab'
+  }
+)
+vim.api.nvim_create_autocmd(
+  { 'FileType' },
+  {
+    pattern = 'confluencewiki,markdown,text,votl',
+    command = 'setlocal spell'
+  }
+)
+
+-- File tree browser
+vim.g.netrw_banner = 0             -- Disable banner
+vim.g.netrw_liststyle = 3          -- Filetree browser style
+vim.g.netrw_altv = 1               -- Split vertically
+vim.g.netrw_bufsettings = 'number' -- Use line numbers
+
+-- Folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldenable = false -- Start unfolded. Re-enabled on fold
+
+-- Search
+if vim.fn.executable('rg') then
+  vim.o.grepprg = 'rg --color=never'
+end
+
+-- Splits
+vim.o.splitbelow = true
+
+-- Terminal
+vim.api.nvim_create_autocmd(
+  { 'TermOpen' },
+  {
+    pattern = '*',
+    command = ':startinsert'
+  }
+)
+
+-- Textwidth
+vim.o.textwidth = 100
+
+-- UI
+vim.o.lazyredraw = true -- Macros don't redraw
+
+-- Visual Bell
+vim.o.visualbell = true
+
+-- Working files
+vim.o.updatetime = 300 -- Write to swap every n ms
