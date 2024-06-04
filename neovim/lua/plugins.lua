ca9ae569aeab52e6a11678af0920bce7392686fd
Eric Slenk <lucid.machine@gmail.com>
2024-06-04 00:15:37 -0400

    nvim: Separate taglist.vim config

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -126,14 +126,7 @@ require('lazy').setup({
   { 'tpope/vim-surround' },
   {
     'vim-scripts/taglist.vim',
-    init = function()
-      vim.g.Tlist_Auto_Highlight_Tag = 1
-      vim.g.Tlist_Auto_Update = 1
-      vim.g.Tlist_Inc_Winwidth = 0
-      vim.g.Tlist_Sort_Type = 'name'
-      vim.g.Tlist_Use_Right_Window = 1
-      vim.g.Tlist_Use_SingleClick = 1
-    end
+    init = function() require('plugins.taglist-vim') end
   },
   { 'wincent/ferret' },
   { 'wincent/loupe' },
