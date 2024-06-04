d7acc19e87f61e5f1059f6e69d847eabf2ad263e
Eric Slenk <lucid.machine@gmail.com>
2024-06-04 00:15:37 -0400

    nvim: Separate taglist.vim config

---
+++ neovim/lua/plugins/taglist-vim.lua
@@ -0,0 +1,6 @@
+vim.g.Tlist_Auto_Highlight_Tag = 1
+vim.g.Tlist_Auto_Update = 1
+vim.g.Tlist_Inc_Winwidth = 0
+vim.g.Tlist_Sort_Type = 'name'
+vim.g.Tlist_Use_Right_Window = 1
+vim.g.Tlist_Use_SingleClick = 1
