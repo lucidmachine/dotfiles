d5deb7583dc44866e4816f03a733ba3c218a1c5e
Eric Slenk <lucid.machine@gmail.com>
2024-06-04 09:06:30 -0400

    nvim: Separate textwidth cycle functions and data

--- neovim/lua/functions.lua
+++ neovim/lua/functions.lua
@@ -1,21 +1,25 @@
 local M = {}
 
+local nextWidths = {
+  [100] = 120,
+  [120] = 80,
+  [80] = 100,
+}
+
+local nextWidth = function(currentWidth)
+  return nextWidths[currentWidth] or 100
+end
+
 --- Cycle the column width
 -- 100 -> 120 -> 80 -> 100
 -- If the current textwidth isn't part of the cycle, then it will restart at 100
-M.cycleColumns = function ()
-  local nextWidths = {
-    [100] = 120,
-    [120] = 80,
-    [80] = 100,
-  }
-  local currentWidth = vim.o.textwidth
-  local nextWidth = nextWidths[currentWidth] or 100
+M.cycleColumns = function()
+  local newWidth = nextWidth(vim.o.textwidth)
 
-  vim.o.textwidth = nextWidth
-  vim.o.colorcolumn = tostring(nextWidth + 1)
+  vim.o.textwidth = newWidth
+  vim.o.colorcolumn = tostring(newWidth + 1)
 
-  vim.print(nextWidth)
+  vim.print(newWidth)
 end
 
 return M
