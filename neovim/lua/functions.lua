e815a780f7b036e2277fde5001b64764319ae89a
Eric Slenk <lucid.machine@gmail.com>
2024-06-09 02:16:37 -0400

    nvim: Port cycle indentation mode function to Lua

--- neovim/lua/functions.lua
+++ neovim/lua/functions.lua
@@ -42,4 +42,43 @@ M.cycleInccommand = function()
 end
 
 
+IndentationMode = 's2'
+
+local indentationModeCycle = {
+  s2 = 's4',
+  s4 = 't2',
+  t2 = 't4',
+  t4 = 's2',
+}
+
+local indentationModeMessages = {
+  s2 = 'Tab = 2 Spaces',
+  s4 = 'Tab = 4 Spaces',
+  t2 = 'Tab = 1 Tab, 2 Spaces Wide',
+  t4 = 'Tab = 1 Tab, 4 Spaces Wide',
+}
+
+local setIndentationMode = function(mode)
+  local tabCharacter = string.sub(mode, 0, 1)
+  local tabWidth = tonumber(string.sub(mode, 1, 2))
+  local expandtab = tabCharacter == 's'
+
+  vim.opt.expandtab = expandtab
+  vim.opt.shiftwidth = tabWidth
+  vim.opt.tabstop = tabWidth
+  IndentationMode = mode
+end
+
+--- Cycle the indentation mode
+-- 2 Spaces -> 4 Spaces -> 2 Space Tab -> 4 Space Tab -> 2 Spaces
+M.cycleIndentationMode = function()
+  local current = IndentationMode
+  local next = advanceCycle(indentationModeCycle, current, 's2')
+  local message = indentationModeMessages[next]
+
+  setIndentationMode(next)
+
+  vim.print(message)
+end
+
 return M
