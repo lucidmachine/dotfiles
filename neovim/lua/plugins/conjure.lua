5ce78a0ef3a7f31d63c956b94eba1e39e4e37630
Eric Slenk <lucid.machine@gmail.com>
2024-06-09 23:44:51 -0400

    nvim: Disable diagnostics in Conjure windows

--- neovim/lua/plugins/conjure.lua
+++ neovim/lua/plugins/conjure.lua
@@ -1,2 +1,9 @@
 vim.g.conjure_log_direction = 'horizontal'
 vim.g['conjure#extract#tree_sitter#enabled'] = true
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  group = vim.api.nvim_create_augroup("conjure_log_disable_lsp", { clear = true }),
+  pattern = { "conjure-log-*" },
+  callback = function() vim.diagnostic.enable(false) end,
+  desc = "Conjure Log disable LSP diagnostics",
+})
