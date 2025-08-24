7750c8ae2bb52ad1e3aa69bab8b31892b7eda5b7
Eric Slenk <lucid.machine@gmail.com>
2025-08-24 11:25:34 -0400

    waybar: next-appointment: Add argument DELTA to script

--- waybar/scripts/next-appointment.sh
+++ waybar/scripts/next-appointment.sh
@@ -3,35 +3,27 @@
 # Gets the next appointment from khal and formats the output for Waybar
 #
 # Synopsis
-#   next-appointment.sh
+#   next-appointment.sh [DELTA]
+#
+# Arguments
+#   DELTA  A (date)time range in the format I{m,h,d}, where I is an integer and m means minutes, h
+#          means hours, and d means days
 #
 # Examples
 #   next-appointment.sh
+#   next-appointment.sh 30m
+#   next-appointment.sh 12h
+#   next-appointment.sh 7d
+
 
 set -euo pipefail
 
-next_appointment="$(
-  2>/dev/null khal list \
+DELTA="${1:-1h}"
+
+2>/dev/null khal list \
     -a personal -a family -a entertainment -a the-gallery \
-    now 1h --notstarted --once \
+    now "${DELTA}" --notstarted --once \
     --json start-time --json title \
     --json end-time --json location --json description \
-  | jq --slurp 'flatten | first'
-)"
-
-if [ "${next_appointment}" == "null" ]; then
-  echo "{ \"text\": null, \"tooltip\": null }"
-  exit 0
-fi
-
-next_appointment_short="$(
-  echo "${next_appointment}" \
-    | jq --raw-output '.["start-time"] + " " + .title'
-)"
-
-next_appointment_long="$(
-  echo "${next_appointment}" \
-    | jq --raw-output '.title + "\r\r " + .["start-time"] + " - " + .["end-time"] + "\r " + .location + "\r " + .description'
-)"
-
-echo "{\"text\": \"${next_appointment_short}\", \"tooltip\": \"${next_appointment_long}\"}"
+  | jq --slurp 'flatten | first' \
+  | jq --compact-output '{ "text": if .? then .["start-time"] + " " + .title end, "tooltip": if .? then .title + "\r\r " + .["start-time"] + " - " + .["end-time"] + "\r " + .location + "\r " + .description end }'
