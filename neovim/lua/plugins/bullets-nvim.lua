c4ea99ebec130c3e4a3e540397b745e634290415
Eric Slenk <lucid.machine@gmail.com>
2025-09-23 15:17:20 -0400

    nvim: Manually specify all bullets.nvim mappings

--- neovim/lua/plugins/bullets-nvim.lua
+++ neovim/lua/plugins/bullets-nvim.lua
@@ -1,20 +1,35 @@
 require('Bullets').setup({
-  outline_levels = {'ROM','ABC', 'num', 'abc', 'rom', 'std*', 'std-'}
+  mappings = false,
+  outline_levels = {'ROM','ABC', 'num', 'abc', 'rom', 'std*', 'std-'},
 })
 
+vim.api.nvim_create_augroup('BulletMaps', { clear = true })
 vim.api.nvim_create_autocmd(
-  'Filetype',
+  { 'Filetype' },
   {
-    pattern = 'markdown,text,gitcommit',
+    pattern = { 'markdown', 'text', 'gitcommit', },
     group = 'BulletMaps',
-    callback = function(_, bufnr)
-      vim.keymap.set('i', '<Tab>', '<Plug>(bullets-demote)', { buffer = bufnr })
-      vim.keymap.set('i', '<S-Tab>', '<Plug>(bullets-promote)', { buffer = bufnr })
-      vim.keymap.set('n', '<LocalLeader>cx', '<Plug>(bullets-toggle-checkbox)', { buffer = bufnr })
+    callback = function(tbl)
+      local buf = tbl.buf
+
+      vim.keymap.set('n', '<LocalLeader>bx', '<Plug>(bullets-toggle-checkbox)', { buffer = buf })
+      vim.keymap.set('n', '<LocalLeader>bn', '<Plug>(bullets-renumber)', { buffer = buf })
+      vim.keymap.set('n', '<<', '<Plug>(bullets-promote)', { buffer = buf })
+      vim.keymap.set('n', '>>', '<Plug>(bullets-demote)', { buffer = buf })
+      vim.keymap.set('n', 'o', '<Plug>(bullets-newline-o)', { buffer = buf })
+
+      vim.keymap.set('i', '<CR>', '<Plug>(bullets-newline-cr)', { buffer = buf })
+      vim.keymap.set('i', '<Tab>', '<Plug>(bullets-demote)', { buffer = buf })
+      vim.keymap.set('i', '<S-Tab>', '<Plug>(bullets-promote)', { buffer = buf })
+
+      vim.keymap.set('v', '<', '<Plug>(bullets-promote)', { buffer = buf })
+      vim.keymap.set('v', '>', '<Plug>(bullets-demote)', { buffer = buf })
+      vim.keymap.set('v', '<LocalLeader>bn', '<Plug>(bullets-renumber)', { buffer = buf })
 
       require('which-key').add({
-        { '<LocalLeader>c', group = '(C)hecklists' },
-        { '<LocalLeader>cx', group = 'Toggle Checkbox' },
+        { '<LocalLeader>b', group = '(B)ullets' },
+        { '<LocalLeader>bx', desc = 'Toggle Checkbo(x)' },
+        { '<LocalLeader>bn', desc = 'Re(n)umber List' },
       })
     end,
   }
