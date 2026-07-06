f7a782c84bf50a2db21edb82367f9fd11df0b4d3
Eric Slenk <lucid.machine@gmail.com>
2026-07-05 22:38:28 -0400

    nvim: Re-enable and configure render-markdown.nvim

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,3 +1,19 @@
 return {
-    { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            heading = { width = "block" },
+            dash = { width = 100 },
+            checkbox = {
+                enabled = true,
+                custom = {
+                    intermediate = {
+                        raw = "[.]",
+                        rendered = "󰡖 ",
+                        highlight = "RenderMarkdownTodo",
+                    },
+                },
+            },
+        },
+    },
 }
